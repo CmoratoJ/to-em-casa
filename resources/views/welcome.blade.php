65b34bab44494acd648bd6e46e0287324e790794
Caio Junqueira <caio@intranet.innovare>
2019-06-21 16:59:18 -0300

    bootstrap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,10 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
+        <link rel="stylesheet" href="css/app.css">
+
+        <script src="js/app.js" charset="utf-8"></script>
+
         <!-- Styles -->
         <style>
             html, body {
